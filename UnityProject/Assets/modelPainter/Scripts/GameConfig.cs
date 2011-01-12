5cd0d8a0e94f4e40df8d5ddc6f2fb89aaa963ea4
unknown <czl@.(none)>
2011-01-13 00:12:38 +0800

    none

---
+++ UnityProject/Assets/modelPainter/Scripts/GameConfig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameConfig
+{
+    public string ModelDir = Application.dataPath + "/../Models";
+
+    static protected GameConfig singletonInstance = new GameConfig();
+
+    public static GameConfig Singleton
+    {
+        get { return singletonInstance; }
+    }
+
+    public static GameConfig getSingleton()
+    {
+        return singletonInstance;
+    }
+
+    //void Awake()
+    //{
+    //    if (singletonInstance!=null)
+    //        Debug.LogError("have singletonInstance");
+    //    singletonInstance = this;
+    //}
+
+}
